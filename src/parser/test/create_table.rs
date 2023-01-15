1cd0c2524e7e5eae74751d98927d830b8ea1facd
myyrakle <sssang97@naver.com>
2023-01-16 00:04:28 +0900

    add: test case

--- src/parser/test/create_table.rs
+++ src/parser/test/create_table.rs
@@ -50,3 +50,50 @@ pub fn create_table() {
         vec![expected],
     );
 }
+
+#[test]
+pub fn create_table_2() {
+    let text = r#"
+        CREATE TABLE "test_db".person
+        (
+            id INTEGER PRIMARY KEY,
+            name varchar(100) NOT NULL,
+            age INTEGER NULL
+        );
+    "#
+    .to_owned();
+
+    let mut parser = Parser::new(text).unwrap();
+
+    let expected = CreateTableQuery::builder()
+        .set_table(TableName::new(
+            Some("test_db".to_owned()),
+            "person".to_owned(),
+        ))
+        .add_column(
+            Column::builder()
+                .set_name("id".to_owned())
+                .set_data_type(DataType::Int)
+                .set_primary_key(true)
+                .build(),
+        )
+        .add_column(
+            Column::builder()
+                .set_name("name".to_owned())
+                .set_data_type(DataType::Varchar(100))
+                .set_not_null(true)
+                .build(),
+        )
+        .add_column(
+            Column::builder()
+                .set_name("age".to_owned())
+                .set_data_type(DataType::Int)
+                .build(),
+        )
+        .build();
+
+    assert_eq!(
+        parser.parse(ParserContext::default()).unwrap(),
+        vec![expected],
+    );
+}
